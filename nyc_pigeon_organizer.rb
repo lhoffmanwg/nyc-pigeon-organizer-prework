4d30d5b0160c3a3dbc252a1ffa24748334efbe14
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-25 00:40:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,15 +6,14 @@ def nyc_pigeon_organizer(data)
   data.each do |detail_key, detail_hash|
     detail_hash.each do |attribute_detail, name|
       name.each do |name_from_ary|
-      # if new_hash == {}
-      #    new_hash[name_from_ary][:color] = detail_ary.push(attribute_detail)
-      #  end
-         
-        binding.pry
+        if (new_hash[name_from_ary][detail_key] == nil)
+          new_hash[name_from_ary][detail_key] = [attribute_detail.to_s()]
+        else
+          new_hash[name_from_ary][detail_key] = new_hash[name_from_ary][detail_key].push(attribute_detail.to_s())
+        end
+       # binding.pry
       end
-    end
-    
-  
+     end
     end  
   return new_hash
   end  
