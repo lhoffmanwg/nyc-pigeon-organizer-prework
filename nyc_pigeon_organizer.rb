aeb23505791443145ebbc317bc4d8cdc75da4472
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-25 01:09:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |detail_key, detail_hash|
+    detail_hash.each do |attribute_detail, name|
+      name.each do |name_from_ary|
+        if (new_hash[name_from_ary] == nil)
+          new_hash[name_from_ary] = {}
+        end
+        if (new_hash[name_from_ary][detail_key] == nil)
+          new_hash[name_from_ary][detail_key] = [attribute_detail.to_s]
+        else
+          new_hash[name_from_ary][detail_key] = new_hash[name_from_ary][detail_key].push(attribute_detail.to_s)
+        end
+      end
+    end
+  end  
+  return new_hash
 end
