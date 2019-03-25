5840cd1cda65ba0f46028b2bc8dee262c3466fe5
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-25 00:55:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |detail_key, detail_hash|
     detail_hash.each do |attribute_detail, name|
       name.each do |name_from_ary|
+        binding.pry
         if (new_hash[name_from_ary] == nil)
           new_hash[name_from_ary] = {}
         end
